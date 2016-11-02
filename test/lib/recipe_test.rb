8ce134b754adeae9ce85c3b2eb7202f4d394598b
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-11-02 13:54:15 -0700

    wrote a test to confirm that recipe.rb's initialize method works as expected. Test is failing

---
+++ test/lib/recipe_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class ChannelTest < ActiveSupport::TestCase
+    test "whether the tests are running" do
+        assert true
+    end
+
+    #
+    # INSTANCE METHODS
+    #
+
+    test "Recipr can be created with name, url, ingredients and dietary info" do
+        name = "test name"
+        url = "test_url.com"
+        ingredients = ["tomatoes", "onions"]
+        dietary_info = ["Vegan", "Gluten-Free"]
+        r = Recipe.new(name, url, ingredients, dietary_info)
+        assert_kind_of Recipe, recipe
+        assert_equal r.name, name
+        assert_equal r.url, url
+        assert_equal r.ingredients, ingredients
+        assert_equal r.dietary_info, dietary_info
+    end
+
+end
