59fde26ca77e7471273ec590ced0bc8091b79861
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-11-02 11:43:01 -0700

    config/application.rb - entered code to automatically load all code from lib directory. Renamed lib/edamam_api_wrapper.rb and in this file started initiating base url

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module ApiMuncher
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Automatically load all code from <rails_root>/lib
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
---
+++ lib/edamam_api_wrapper.rb
@@ -0,0 +1,11 @@
+require `httparty`
+
+class RecipeApiWrapper
+    BASE_URL = "https://api.edamam.com/search?q="
+    ID = ENV["id"]
+    KEY = ENV["key"]
+
+    def self.search(query_term)
+
+    end
+end
