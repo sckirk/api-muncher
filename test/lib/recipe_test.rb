d54c8448c7d3e29e66390742ec3333f40e7ce169
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-11-02 16:33:32 -0700

    wrote the test and for recipe.rb's initialize method--test is passing

--- test/lib/recipe_test.rb
+++ test/lib/recipe_test.rb
@@ -24,4 +24,21 @@ class ChannelTest < ActiveSupport::TestCase
         assert_equal r.dietary_info, dietary_info
     end
 
+    test "ArgumentError will be raised and Recipe object will not be created if it's missing one or more parameters" do
+        name = "test name"
+        image = "https://www.edamam.com/web-img/108/108bb897a2fd724bc1461d77031a560f.jpg"
+        url = "test_url.com"
+        ingredients = ["tomatoes", "onions"]
+        dietary_info = ["Vegan", "Gluten-Free"]
+
+        assert_raises ArgumentError do
+            missing_name = Recipe.new(image, url, ingredients, dietary_info)
+            return missing_name
+        end
+
+        assert_raises ArgumentError do
+            missing_image_and_ingredients = Recipe.new(name, url, dietary_info)
+            return missing_image_and_ingredients
+        end
+    end
 end
