378b33fa6cdf723e11929cb2a538bad33f0a60d7
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-11-02 13:21:21 -0700

    debugged. Edamam.search_results test now passes.

--- lib/edamam_api_wrapper.rb
+++ lib/edamam_api_wrapper.rb
@@ -1,12 +1,12 @@
-require `httparty`
+require 'httparty'
 
-class RecipeApiWrapper
+class EdamamApiWrapper
     BASE_URL = "https://api.edamam.com/search?q="
     ID = ENV["id"]
     KEY = ENV["key"]
 
     def self.search_results(query_term)
-        url = BASE_URL + query_term + "&app_id=#{ id }&app_key=#{ key }"
+        url = BASE_URL + query_term + "&app_id=#{ ID }&app_key=#{ KEY }"
         data = HTTParty.get(url)
         recipe_results = []
         if data["hits"]
@@ -15,5 +15,6 @@ class RecipeApiWrapper
                 recipe_results << wrapper
             end
         end
+        return recipe_results
     end
 end
