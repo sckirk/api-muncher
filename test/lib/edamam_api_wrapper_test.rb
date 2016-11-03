c1a67b56d53ffc58640d253027d2c6f78d76d71e
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-11-03 14:45:00 -0700

    wrote a negative case for an invalid search term--it passes

--- test/lib/edamam_api_wrapper_test.rb
+++ test/lib/edamam_api_wrapper_test.rb
@@ -5,7 +5,7 @@ class EdamamApiWrapperTest < ActiveSupport::TestCase
         assert true
     end
 
-    test "EdamamApiWrapper.search_results will return an array of Recipes" do
+    test "EdamamApiWrapper.search_results will return an array of Recipes on a valid search term" do
         VCR.use_cassette("recipes") do
             recipes = EdamamApiWrapper.search_results("avocado")
             assert_kind_of Array, recipes
@@ -13,6 +13,14 @@ class EdamamApiWrapperTest < ActiveSupport::TestCase
             recipes.each do |recipe|
                 assert_kind_of Recipe, recipe
             end
+        end
+    end
+
+    test "EdamamApiWrapper.search_results will return an empty array on an invalid search term" do
+        VCR.use_cassette("recipes") do
+            recipes = EdamamApiWrapper.search_results("gobblygook")
+            assert_kind_of Array, recipes
+            assert recipes.empty?
         end
     end
 
