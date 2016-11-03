528e9ee2ea70a275a93f0fb4bb39e02ecf89e12b
Suzannah Kirk-Daligcon <skirk.seattle@gmail.com>
2016-11-02 22:50:05 -0700

    created a session[:search] in the homepages controller to HOLD ONTO the search query from the index method to use in the show method... it works! Show page is doing all that it needs to. Yay. So fun to figure out how to open the recipe link in a new tab on the show page. One show test is passing--the other one is failing because I haven't yet figured out how to pass it the correct parameters.

--- app/controllers/homepages_controller.rb
+++ app/controllers/homepages_controller.rb
@@ -1,9 +1,16 @@
 class HomepagesController < ApplicationController
     def index
         if params[:search]
+            session[:search] = params[:search]
             @results = Recipe.results(params[:search])
         end
     end
 
-    def show;end
+    def show
+        if session[:search].nil?
+            redirect_to root_path
+        else
+            @result = Recipe.by_name(session[:search], params[:name])
+        end
+    end
 end
